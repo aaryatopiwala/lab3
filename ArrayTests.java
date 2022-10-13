2344707158b5f8be8911dda43c4523cc573d237d
T-r-i-p-l-e-A <75400069+aaryatopiwala@users.noreply.github.com>
2022-10-13 12:47:10 -0700

    Fixed ArrayExamples methods and added tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+	public void testReverseInPlaceMultiple() {
+    int[] input1 = { 3,2,1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 1,2,3 }, input1);
+	}
+
+  @Test 
+	public void testReverseMultiple() {
+    int[] input1 = { 3,2,1 };
+    int[] output1 = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{ 1,2,3 }, output1);
+	}
 }
